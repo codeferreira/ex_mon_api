9152a9e949c60672311706a8582463f7db5c9899
José Ferreira <dev.josejunior@gmail.com>
2023-03-30 23:43:25 -0300

    feat: create trainer builder functions

--- lib/ex_mon_api/trainer.ex
+++ lib/ex_mon_api/trainer.ex
@@ -5,8 +5,35 @@ defmodule ExMonApi.Trainer do
   @primary_key {:id, Ecto.UUID, autogenerate: true}
 
   schema "trainers" do
-    field :name, :string
-    field :password_hash, :string
+    field(:name, :string)
+    field(:password_hash, :string)
+    field(:password, :string, virtual: true)
     timestamps()
   end
+
+  @required_fields [:name, :password]
+
+  def build(params) do
+    params
+    |> changeset()
+    |> apply_action(:insert)
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @required_fields)
+    |> validate_required(@required_fields)
+    |> validate_length(:password, min: 6)
+  end
+
+  def handle_password_hash(
+        %Ecto.Changeset{
+          valid?: true,
+          changes: %{password: password}
+        } = changeset
+      ) do
+    change(changeset, Argon2.add_hash(password))
+  end
+
+  def handle_password_hash(changeset), do: changeset
 end
