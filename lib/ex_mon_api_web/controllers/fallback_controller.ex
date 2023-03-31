3e2ea0111ec8c2062461ea71cabc3db2158e1398
José Ferreira <dev.josejunior@gmail.com>
2023-03-31 19:49:20 -0300

    refactor: response handler to receive proper status

--- lib/ex_mon_api_web/controllers/fallback_controller.ex
+++ lib/ex_mon_api_web/controllers/fallback_controller.ex
@@ -1,9 +1,9 @@
 defmodule ExMonApiWeb.FallbackController do
   use ExMonApiWeb, :controller
 
-  def call(conn, {:error, result}) do
+  def call(conn, {:error, result, status}) do
     conn
-    |> put_status(:bad_request)
+    |> put_status(status)
     |> put_view(ExMonApiWeb.ErrorJSON)
     |> render(:bad_request, result: result)
   end
--- lib/ex_mon_api_web/controllers/trainers_controller.ex
+++ lib/ex_mon_api_web/controllers/trainers_controller.ex
@@ -4,16 +4,24 @@ defmodule ExMonApiWeb.TrainersController do
   action_fallback ExMonApiWeb.FallbackController
 
   def create(conn, params) do
-    params
-    |> ExMonApi.create_trainer()
-    |> handle_response(conn)
+    result =
+      params
+      |> ExMonApi.create_trainer()
+
+    result =
+      case result do
+        {:ok, struct} -> {:ok, struct, :created}
+        {:error, changeset} -> {:error, changeset, :unprocessable_entity}
+      end
+
+    handle_response(result, conn)
   end
 
-  defp handle_response({:ok, struct}, conn) do
+  defp handle_response({:ok, struct, status}, conn) do
     conn
-    |> put_status(:created)
+    |> put_status(status)
     |> render(:create, trainer: struct)
   end
 
-  defp handle_response({:error, _changeset} = error, _conn), do: error
+  defp handle_response({:error, _changeset, _status} = error, _conn), do: error
 end
