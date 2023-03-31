383eebea8475d590f742235a7e7aa60f908685e2
José Ferreira <dev.josejunior@gmail.com>
2023-03-30 23:04:30 -0300

    feat: add trainer module

---
+++ lib/ex_mon_api/trainer.ex
@@ -0,0 +1,12 @@
+defmodule ExMonApi.Trainer do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, Ecto.UUID, autogenerate: true}
+
+  schema "trainers" do
+    field :name, :string
+    field :password_hash, :string
+    timestamps()
+  end
+end
