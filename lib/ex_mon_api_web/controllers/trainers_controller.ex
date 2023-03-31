479b0e936569402f95ac75be52062036e509b7a8
José Ferreira <dev.josejunior@gmail.com>
2023-03-31 20:09:37 -0300

    feat: add delete handler

--- lib/ex_mon_api_web/controllers/trainers_controller.ex
+++ lib/ex_mon_api_web/controllers/trainers_controller.ex
@@ -4,24 +4,39 @@ defmodule ExMonApiWeb.TrainersController do
   action_fallback ExMonApiWeb.FallbackController
 
   def create(conn, params) do
-    result =
-      params
-      |> ExMonApi.create_trainer()
-
-    result =
-      case result do
-        {:ok, struct} -> {:ok, struct, :created}
-        {:error, changeset} -> {:error, changeset, :unprocessable_entity}
-      end
-
-    handle_response(result, conn)
+    params
+    |> ExMonApi.create_trainer()
+    |> handle_status(:created, :bad_request)
+    |> handle_create(conn)
   end
 
-  defp handle_response({:ok, struct, status}, conn) do
+  def delete(conn, %{"id" => id}) do
+    id
+    |> ExMonApi.delete_trainer()
+    |> handle_status(:no_content, :not_found)
+    |> handle_delete(conn)
+  end
+
+  defp handle_delete({:ok, _struct, status}, conn) do
+    conn
+    |> put_status(status)
+    |> text("")
+  end
+
+  defp handle_delete({:error, _reason, _status} = error, _conn), do: error
+
+  defp handle_create({:ok, struct, status}, conn) do
     conn
     |> put_status(status)
     |> render(:create, trainer: struct)
   end
 
-  defp handle_response({:error, _changeset, _status} = error, _conn), do: error
+  defp handle_create({:error, _changeset, _status} = error, _conn), do: error
+
+  defp handle_status(response, right_status, wrong_status) do
+    case response do
+      {:ok, struct} -> {:ok, struct, right_status}
+      {:error, changeset} -> {:error, changeset, wrong_status}
+    end
+  end
 end
